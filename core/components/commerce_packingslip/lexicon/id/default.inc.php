eec642b279fd220434047a3dbefdce2df86593f7
Mark Hamstra <hello@markhamstra.com>
2018-03-06 22:34:07 +0100

    Lexicon sync

---
+++ core/components/commerce_packingslip/lexicon/id/default.inc.php
@@ -0,0 +1,6 @@
+<?php
+
+$_lang['commerce_packingslip'] = 'Packing Slip';
+$_lang['commerce_packingslip.description'] = 'Adds an action to order shipments to create a packing slip.';
+$_lang['commerce_packingslip.print_slip'] = 'Print Packing Slip';
+$_lang['commerce_packingslip.packing_slip_for'] = 'Packing slip for Order [[+order]]';
---
+++ core/components/commerce_packingslip/lexicon/nl/default.inc.php
@@ -0,0 +1,6 @@
+<?php
+
+$_lang['commerce_packingslip'] = 'Pakbon';
+$_lang['commerce_packingslip.description'] = 'Voegt een actie toe aan verzendingen om een pakbon te genereren.';
+$_lang['commerce_packingslip.print_slip'] = 'Print pakbon';
+$_lang['commerce_packingslip.packing_slip_for'] = 'Pakbon voor bestelling [[+order]]';
