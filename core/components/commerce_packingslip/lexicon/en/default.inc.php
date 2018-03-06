931f8ea0dbebafdd42bde91d4724bb0bacdd5b10
Mark Hamstra <hello@markhamstra.com>
2018-03-06 22:00:58 +0100

    Use lexicons and add some minor styling tweaks (font, spacing)

--- core/components/commerce_packingslip/lexicon/en/default.inc.php
+++ core/components/commerce_packingslip/lexicon/en/default.inc.php
@@ -3,3 +3,4 @@
 $_lang['commerce_packingslip'] = 'Packing Slip';
 $_lang['commerce_packingslip.description'] = 'Adds an action to order shipments to create a packing slip.';
 $_lang['commerce_packingslip.print_slip'] = 'Print Packing Slip';
+$_lang['commerce_packingslip.packing_slip_for'] = 'Packing slip for Order [[+order]]';
